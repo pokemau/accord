b5b7f3c36623aadc2bd44f8860a0ef29e5aed5ef
pokemau <rentillosa90@gmail.com>
2024-05-06 20:32:07 +0800

    get checkbox value func

--- js/imports/utilities.js
+++ js/imports/utilities.js
@@ -1,4 +1,4 @@
-export function showPopUpDialog(popUpDialog){
+export function showPopUpDialog(popUpDialog) {
     popUpDialog[0].show();
     popUpDialog.css({
         display: "flex",
@@ -8,26 +8,30 @@ export function showPopUpDialog(popUpDialog){
     });
 }
 
-export function hidePopUpDialog(popUpDialog){
+export function hidePopUpDialog(popUpDialog) {
     popUpDialog.css({
-        display: "none"
+        display: "none",
     });
     popUpDialog[0].close();
 }
 
-export function hideAllPopUpDialog(){
+export function hideAllPopUpDialog() {
     $("dialog").css({
-        display: "none"
+        display: "none",
     });
     $("dialog")[0].close();
 }
 
-export function showMessage(message){
-    $("body").append("<div id='message-box'> <p>"+message+"</p> </div>");
-    setTimeout(function() {
-        var messageBox = document.getElementById('message-box');
+export function showMessage(message) {
+    $("body").append("<div id='message-box'> <p>" + message + "</p> </div>");
+    setTimeout(function () {
+        var messageBox = document.getElementById("message-box");
         if (messageBox) {
-          messageBox.parentNode.removeChild(messageBox);
+            messageBox.parentNode.removeChild(messageBox);
         }
-      }, 3000);
+    }, 3000);
+}
+
+export function getCheckboxValue(checkbox) {
+    return checkbox.is(":checked") ? 1 : 0;
 }
