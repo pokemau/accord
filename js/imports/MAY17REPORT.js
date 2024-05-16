5936b8063edc26254d50944d5b85f86a0bda8445
pokemau <rentillosa90@gmail.com>
2024-05-16 21:35:22 +0800

    3 reports

--- js/imports/MAY17REPORT.js
+++ js/imports/MAY17REPORT.js
@@ -11,5 +11,48 @@ export async function getReportData_17() {
 }
 
 function printReportData(allData) {
+    const div = $("#report-main-cont");
     console.log(allData);
+    const serverCount = allData.serverCount;
+    const userCount = allData.userCount;
+    const messages = allData.msgCount;
+
+    console.log(`Server Count: ${serverCount}`);
+    console.log(`User Count: ${userCount}`);
+
+    let string = `
+        <h2>Total number of servers: ${serverCount}</h2>
+        <h2>Total number of users: ${userCount}</h2>
+        
+        <h2>Messages per server:</h2>
+
+        <table id='report-table'>
+            <thead>
+                <tr>
+                    <th>Server Name</th>
+                    <th>Total messages</th>
+                </tr>
+            </thead>
+
+            <tbody>
+    `;
+
+    messages.forEach((msg, i) => {
+        string += `
+            <tr>
+                <td>${msg.servername}</td>
+                <td>${msg.totalMessages}</td>
+            </tr>
+        `;
+        // console.log(`${msg.servername}\nMessage Count: ${msg.totalMessages}`);
+    });
+
+    string += `
+    
+            
+        </tbody>
+        </table>
+    `;
+
+    $(string).appendTo(div);
 }
--- report.php
+++ report.php
@@ -6,10 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script type="module" src="js/imports/MAY17REPORT.js" defer></script>
+  <link rel="stylesheet" href="css/report.css" />
   <title>Accord Report</title>
 </head>
 
 <body>
+
+  <div id="report-main-cont">
+
+  </div>
 
 </body>
 
